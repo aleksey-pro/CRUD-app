a7c1b53d33fe27aeb5661902288e87159dac2550
daveghan <anxieter@yandex.ru>
2017-06-12 15:10:20 +0300

    Initial commit

---
+++ app/routes/index.js
@@ -0,0 +1,5 @@
+'use strict';
+const noteRoutes = require('./note_routes');
+module.exports = function(app, db) {
+	noteRoutes(app, db);
+};
---
+++ config/db.js
@@ -0,0 +1,4 @@
+'use strict';
+module.exports = {
+	url: 'mongodb://root:181621@ds111262.mlab.com:11262/portfolio'
+}
